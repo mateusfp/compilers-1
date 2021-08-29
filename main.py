d8b649fadb9ab3fc2869b95cc67719cbf362865f
Mateus Santos <ms825957@gmail.com>
2021-08-29 15:53:14 -0300

    Mateus Santos

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 email = 'guihss.cs@gmail.com'
 
 
@@ -45,4 +46,42 @@ def verify_date(date):
 def toInt(number):
 
     pass
+
+#Crie um programa que verifica se uma data está correta
+
+def data(dia, mes, ano):
+    dia = int(input("Digite O Dia: "))
+    mes = int(input("Digite o Mês: "))
+    ano = int(input("Digite o Ano: "))
+
+    if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+        M_Dias = 31
+    elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+        M_Dias = 30
+    elif ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+        M_Dias = 29
+    else:
+        M_Dias = 28
+
+    if mes < 1 or mes > 12:
+        print("Data Inválida: ")
+    elif dia < 1 or dia > M_Dias:
+        print("Data inválida")
+    else:
+        print("Data Válida")
+
+
+
+
+
+#Crie um programa que converta de string para números inteiros
+
+
+string1 = '12,20,21,22,23,24,25'
+
+int_list = list(map(int,string1.split(',')))
+print(int_list)
+print(string1.isnumeric())
+print('----------------------')
+
 
